b3d3d0142cac1eedcc952fc1906ac46b7a55dfff
FredrikAngstrom <fredrik.ang@gmail.com>
2019-02-06 14:29:45 +0100

    Hårdvaruinit processor två

--- TSIU51-1/TSIU51-1/main.asm
+++ TSIU51-1/TSIU51-1/main.asm
@@ -5,8 +5,22 @@
 ; Author : Jensg
 ;
 
+COLD:
+	ldi	 r16,HIGH(RAMEND)
+	out		SPH,r16
+	ldi		r16,LOW(RAMEND)
+	out		SPL,r16
+	rcall	INIT
 
-; Replace with your application code
-start:
-    inc r16
-    rjmp start
+	ldi		r16,$FF
+	out		DDRA,r16
+	ldi		r16,$B7
+	out		DDRB,r16
+	ldi		r16,$80
+	out		DDRC,r18
+	ldi		r16,$00
+	out		DDRD,r16
+	ret
+
+
+
