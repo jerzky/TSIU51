b4aaa24cbd7ac8094c7179b04d5fe77190a86c7a
FredrikAngstrom <fredrik.ang@gmail.com>
2019-02-06 14:21:09 +0100

    hårvaruinit processor 2

--- TSIU51-2/TSIU51-2/main.asm
+++ TSIU51-2/TSIU51-2/main.asm
@@ -7,6 +7,24 @@
 
 
 ; Replace with your application code
-start:
-    inc r16
-    rjmp start
+
+COLD:
+	ldi	 r16,HIGH(RAMEND)
+	out		SPH,r16
+	ldi		r16,LOW(RAMEND)
+	out		SPL,r16
+	rcall	INIT
+
+INIT:
+	ldi		r16,$FF
+	out		DDRA,r16
+	ldi		r16,$40
+	out		DDRB,r16
+	ldi		r16,$80
+	out		DDRC,r18
+	ldi		r16,$00
+	out		DDRD,r16
+	ret
+
+
+
