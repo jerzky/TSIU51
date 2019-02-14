fa353ae45c155efd0b40f4db6b1972292f29afbe
jerzky <jensg90@gmail.com>
2019-02-14 12:24:30 +0100

    Lade till funktion för loopa alla bokstäver

--- TSIU51-2/TSIU51-2/main.asm
+++ TSIU51-2/TSIU51-2/main.asm
@@ -8,19 +8,12 @@
 
 ; Replace with your application code
 
-;using 1026
+;using 1026 -> 490
 
 	
 
 
 	.equ PULSE_TABLE_WIDTH = 8
-	
-	.macro		MOVE
-		ldi		r16, @0
-		ldi		r24, LOW(@1)
-		ldi		r25, HIGH(@1)
-		rcall	MOVE_FUNC
-	.endmacro
 
 
 	.org 0000
@@ -37,26 +30,16 @@ SEVENTH_PULSE:	.db $40, $90, $04, $09, $44, $49, $94, $99
 EIGHT_PULSE:	.db $50, $10, $05, $01, $55, $51, $15, $11
 
 
-	.equ NORTH = 0 
-	.equ SOUTH = 1 
-	.equ EAST = 2 
-	.equ WEST =  3
-	.equ NORTH_EAST = 4
-	.equ NORTH_WEST = 5
-	.equ SOUTH_EAST = 6
-	.equ SOUTH_WEST = 7
-
-	.equ FONT_HEIGHT = 16 ; MUST BE DIVISIBLE BY 8
-	.equ FONT_WIDTH = 8 ; MUST BE DIVISIBLE BY 8
-
 //	0         0        0  0     0 0       0    0
 // PENUP/DOWN                       DIRECTION  
 
 
 //                    1                2                3                4                5                6                7                8                9                10                11                12                13
-ALPHA_A: .db	$80, $00, $0E,   $84, $00, $02,   $28, $00, $04,   $86, $00, $02,   $81, $00, $04,   $83, $00, $08,   $82, $00, $08,   $01, $00, $0A,   $FF, $00
+ALPHA_A: .db	$80, $00, $0E,	 $84, $00, $02,	  $82, $00, $04,   $86, $00, $02,   $81, $00, $06,   $83, $00, $08,   $02, $00, $08,   $81, $00, $08, $FF
 
-ALPHA_B: .db	$80, $00, $10,   $82, $00, $08,   $81, $00, $04,   $87, $00, $02,   $83, $00, $04,   $82, $00, $04,   $86, $00, $02,   $81, $00, $08,   $83, $00, $08,   $82, $00, $08,   $FF, $00
+ALPHA_B: .db $80, $00, $10, $82, $00, $06, $86, $00, $02, $81, $00, $04, $87, $00, $02, $83, $00, $06, $02, $00, $06, $86, $00, $02, $81, $00, $04, $87, $00, $02, $83, $00, $06, $02, $00, $08, $FF
+
+;ALPHA_B: .db	$80, $00, $10,   $82, $00, $08,   $81, $00, $04,   $87, $00, $02,   $83, $00, $04,   $82, $00, $04,   $86, $00, $02,   $81, $00, $08,   $83, $00, $08,   $82, $00, $08,   $FF, $00
 
 ALPHA_C: .db	$00, $00, $04,   $80, $00, $08,   $84, $00, $04,   $82, $00, $04,   $01, $00, $10,   $83, $00, $04,   $85, $00, $04,   $02, $00, $08,   $01, $00, $04,   $FF
 
@@ -68,12 +51,102 @@ ALPHA_F: .db  	$80, $00, $10,   $82, $00, $08,   $01, $00, $06,   $83, $00, $08,
 
 ALPHA_G: .db	$00, $00, $04,   $80, $00, $08,   $84, $00, $04,   $82, $00, $04,   $01, $00, $10,   $83, $00, $04,   $85, $00, $04,   $02, $00, $08,   $01, $00, $04,   $80, $00, $08,   $83, $00, $04,   $02, $00, $04,   $01, $00, $08,   $FF
 
+ALPHA_H: .db    $80, $00, $10,   $01, $00, $08,   $82, $00, $08,   $80, $00, $08,   $01, $00, $08,   $81, $00, $08,   $FF
+
+ALPHA_I: .db	$02, $00, $06, $82, $00, $02, $80, $00, $10, $83, $00, $02, $02, $00, $02, $82, $00, $02, $01, $00, $10, $83, $00, $02, $02, $00, $08, $FF
+
+ALPHA_J: .db	$00, $00, $10, $82, $00, $08, $81, $00, $0E, $87, $00, $02, $83, $00, $04, $85, $00, $02, $02, $00, $08, $01, $00, $02, $FF
+
+ALPHA_K: .db	$80, $00, $10, $02, $00, $08, $87, $00, $08, $86, $00, $08, $FF
+
+ALPHA_L: .db	$04, $00, $08, $00, $00, $08, $81, $00, $10, $82, $00, $08, $FF
+
+ALPHA_M: .db	$80, $00, $10, $82, $00, $01, $81, $00, $02, $82, $00, $01, $81, $00, $02, $82, $00, $01, $81, $00, $02, $82, $00, $01, $81, $00, $02, $80, $00, $02, $82, $00, $01, $80, $00, $02, $82, $00, $01, $80, $00, $02, $82, $00, $01, $80, $00, $02, $82, $00, $01, $81, $00, $10, $FF
+
+ALPHA_N: .db	$80, $00, $10, $82, $00, $01, $81, $00, $02, $82, $00, $01, $81, $00, $02, $82, $00, $01, $81, $00, $02, $82, $00, $01, $81, $00, $02, $82, $00, $01, $81, $00, $02, $82, $00, $01, $81, $00, $02, $82, $00, $01, $81, $00, $02, $82, $00, $01, $81, $00, $02, $80, $00, $10, $01, $00, $10, $FF
+
+ALPHA_O: .db	$00, $00, $02, $80, $00, $0C, $84, $00, $02, $82, $00, $04, $86, $00, $02, $81, $00, $0C, $87, $00, $02, $83, $00, $04, $85, $00, $02, $01, $00, $02, $02, $00, $08, $FF
+
+ALPHA_P: .db	$80, $00, $10, $82, $00, $06, $86, $00, $02, $81, $00, $04, $87, $00, $02, $83, $00, $06, $02, $00, $08, $01, $00, $08, $FF
+
+ALPHA_Q: .db	$00, $00, $02,		$80, $00, $0C,		$84, $00, $02,		$82, $00, $04,		$86, $00, $02,		$81, $00, $0C,		$87, $00, $02,		$83, $00, $04,		$85, $00, $02,		$01, $00, $02,		$02, $00, $08, $85, $00, $04, $06, $00, $04, $FF
+
+ALPHA_R: .db	$80, $00, $10,		$82, $00, $06,		$86, $00, $02,		$81, $00, $04,		$87, $00, $02,		$83, $00, $06,		$86, $00, $08,		$FF
+
+ALPHA_S: .db	$00, $00, $06, $81, $00, $04, $82, $00, $01, $81, $00, $01, $82, $00, $01, $81, $00, $01, $82, $00, $04, $80, $00, $01, $82, $00, $01, $80, $00, $01, $82, $00, $01, $80, $00, $04, $83, $00, $01, $80, $00, $01, $83, $00, $01, $80, $00, $01, $83, $00, $01, $80, $00, $01, $83, $00, $01, $80, $00, $01, $83, $00, $01, $80, $00, $01, $83, $00, $01, $80, $00, $01, $82, $00, $01, $80, $00, $01, $82, $00, $01, $80, $00, $01, $82, $00, $04, $FF
+					
+ALPHA_ALL: .dw  ALPHA_A, ALPHA_B, ALPHA_C, ALPHA_D, ALPHA_E, ALPHA_F, ALPHA_G, ALPHA_H, ALPHA_I, ALPHA_J, ALPHA_K, ALPHA_L, ALPHA_M, ALPHA_N, ALPHA_O, ALPHA_P, ALPHA_Q, ALPHA_R, ALPHA_S, $FF
+
+.equ	 ADDR = ALPHA_J*2
+INIT:
+		
+		ldi		r16, HIGH(RAMEND)
+		out		SPH, r16
+		ldi		r16, LOW(RAMEND)
+		out		SPL, r16
+		ldi		r16, $FF
+		out		DDRA, r16
+
+		ldi		r16, $80
+		out		DDRC, r16
+START:
+		rcall	TEST_ALL
+		//rcall	TEST_DRAW
+END:
+		jmp	end
 
 
+TEST_ALL:
+			ldi		ZH, HIGH(ALPHA_ALL*2)
+			ldi		ZL, LOW(ALPHA_ALL*2)
+TEST_ALL_LOOP:
+			lpm		r16, Z+
+			cpi		r16, $FF
+			breq	TEST_ALL_DONE
+			lpm		r17, Z+
+			push	ZL
+			push	ZH
+			lsl		r17
+			lsl		r16
+			brcc	NO_CARRY
+			subi	r17, -1
+NO_CARRY:
+			mov		ZH, r17
+			mov		ZL, r16
+			rcall	DRAW_FUNC
+			pop		ZH
+			pop		ZL
+			//För att ha lite mellan bokstäverna			
+			ldi		r16, $02
+			ldi		r25, $00
+			ldi		r24, $0A
+			rcall	MOVE_FUNC
+			//------------
+			rjmp	TEST_ALL_LOOP
+TEST_ALL_DONE:
+			ret
+
+
+DRAW_FUNC:		
+			lpm		r16, Z+
+			cpi		r16, $FF
+			breq	DRAW_FUNC_DONE
+			sbrc	r16, 7
+			rcall	LOWER_PEN
+			sbrs	r16, 7
+			rcall	RAISE_PEN
+			andi	r16, $7F
+			lpm		r25, Z+
+			lpm		r24, Z+
+			rcall	MOVE_FUNC
+			rjmp	DRAW_FUNC					
+DRAW_FUNC_DONE:
+			rcall	RAISE_PEN
+			ret		
 
 TEST_DRAW:
-			ldi		ZH, HIGH(ALPHA_G*2)
-			ldi		ZL, LOW(ALPHA_G*2)
+			ldi		ZH, HIGH(ADDR)
+			ldi		ZL, LOW(ADDR)
 	
 TEST_DRAW_LOOP:			
 			lpm		r16, Z+
@@ -121,85 +194,9 @@ MOVE_FUNC_LOOP:
 
 
 
-INIT:
-		
-		ldi		r16, HIGH(RAMEND)
-		out		SPH, r16
-		ldi		r16, LOW(RAMEND)
-		out		SPL, r16
-		ldi		r16, $FF
-		out		DDRA, r16
 
-		ldi		r16, $80
-		out		DDRC, r16
-START:
-		rcall	TEST_DRAW
-END:
-		jmp	end
 
-	DRAW_H:
-		rcall	LOWER_PEN
-		MOVE	NORTH, FONT_HEIGHT
-		MOVE	SOUTH, FONT_HEIGHT / 2
-		MOVE	EAST, FONT_WIDTH
-		MOVE	NORTH, FONT_HEIGHT / 2
-		MOVE	SOUTH,	FONT_HEIGHT
-		rcall	RAISE_PEN
-		ret
-	DRAW_I:
-		MOVE	EAST, FONT_WIDTH / 4
-		rcall	LOWER_PEN
-		MOVE	EAST, FONT_WIDTH / 4
-		MOVE	NORTH, FONT_HEIGHT
-		MOVE	WEST, FONT_WIDTH / 4
-		rcall	RAISE_PEN
-		MOVE	EAST, FONT_WIDTH / 2
-		rcall	LOWER_PEN
-		MOVE	WEST, FONT_WIDTH / 4
-		rcall	RAISE_PEN
-		MOVE	SOUTH, FONT_HEIGHT
-		rcall	LOWER_PEN
-		MOVE	EAST, FONT_WIDTH / 4
-		rcall	RAISE_PEN
-		MOVE	EAST, FONT_WIDTH / 4
-		ret
-	DRAW_J:
-		MOVE	NORTH, FONT_HEIGHT
-		MOVE	EAST, FONT_WIDTH / 4
-		rcall	LOWER_PEN
-		MOVE	EAST, FONT_WIDTH-(FONT_WIDTH / 4)
-		MOVE	SOUTH, FONT_HEIGHT-(FONT_HEIGHT / 8)
-		MOVE	SOUTH_WEST, FONT_WIDTH / 4
-		MOVE	WEST, FONT_WIDTH - (FONT_HEIGHT / 4)
-		MOVE	NORTH_WEST,	FONT_WIDTH / 4
-		rcall	RAISE_PEN
-		MOVE	NORTH_EAST, FONT_WIDTH / 4
-		MOVE	EAST, FONT_WIDTH - (FONT_HEIGHT / 4)
-		MOVE	SOUTH_EAST,	FONT_WIDTH / 4
-		MOVE	SOUTH, FONT_HEIGHT / 8
-		ret
-	DRAW_K:
-		MOVE	NORTH, FONT_HEIGHT
-		rcall	LOWER_PEN
-		MOVE	SOUTH, FONT_HEIGHT/2
-		MOVE	EAST, FONT_WIDTH/4
-		MOVE	NORTH_EAST, (FONT_WIDTH/2)+(FONT_WIDTH/4)
-		MOVE	NORTH, FONT_WIDTH/4
-		rcall	RAISE_PEN
-		MOVE	SOUTH, FONT_WIDTH/4
-		MOVE	SOUTH_WEST,	(FONT_WIDTH/2)+(FONT_WIDTH/4)
-		rcall	LOWER_PEN
-		MOVE	SOUTH_EAST, (FONT_WIDTH/2)+(FONT_WIDTH/4)
-		MOVE	SOUTH, FONT_WIDTH/4
-		rcall	RAISE_PEN
-		MOVE	NORTH, FONT_WIDTH/4
-		MOVE	NORTH_WEST, (FONT_WIDTH/2)+(FONT_WIDTH/4)
-		MOVE	WEST, FONT_WIDTH/4
-		rcall	LOWER_PEN
-		MOVE	SOUTH, FONT_HEIGHT/2
-		rcall	RAISE_PEN
-		MOVE	EAST, FONT_WIDTH
-		ret
+
 
 
 LOWER_PEN:
